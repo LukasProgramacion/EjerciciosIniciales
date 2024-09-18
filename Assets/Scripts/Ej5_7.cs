db600c0cf01d99e92d21358fc62073b1925405c5
LukasProgramacion <lukas.arts315@gmail.com>
2024-09-18 19:57:11 +0200

    18/09 Descanso

--- Assets/Scripts/Ej5_7.cs
+++ Assets/Scripts/Ej5_7.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 
 public class Ej5_7 : MonoBehaviour
 {
-    [SerializeField] int velocidadJugador1, velocidadJugador2, velocidadJugador3;
+    [SerializeField] int velocidadJugador1 = 50, velocidadJugador2 = 25, velocidadJugador3 = 75;
     // Start is called before the first frame update
     void Start()
     {
         if (velocidadJugador1 > velocidadJugador2 && velocidadJugador1 > velocidadJugador3 )
         {
-           //mas rapido el 1
+            Debug.Log("La velocidad del jugador 1, " + velocidadJugador1 + " es la más rapida");
             if (velocidadJugador2 > velocidadJugador3)
            {
-                // 1, luego 2, luego 3
+                Debug.Log("La velocidad del jugador 1, " + velocidadJugador1 + " es la más rapida, después la del 2: " + velocidadJugador2 + " y despues la del 3:" + velocidadJugador3);
            }
            else
            {
-                // 1, luego 3, luego 2
-           }            
+                Debug.Log("La velocidad del jugador 1, " + velocidadJugador1 + " es la más rapida, después la del 3: " + velocidadJugador3 + " y despues la del 2:" + velocidadJugador2);
+            }            
         }
         else if (velocidadJugador2 > velocidadJugador1 && velocidadJugador2 > velocidadJugador3)
         {
-            //mas rapido el 2
+            Debug.Log("La velocidad del jugador 2, " + velocidadJugador2 + " es la más rapida");
             if (velocidadJugador1 > velocidadJugador3)
             {
-                //2, luego 1, luego 3
+                Debug.Log("La velocidad del jugador 2, " + velocidadJugador2 + " es la más rapida, después la del 1: " + velocidadJugador1 + " y despues la del 3:" + velocidadJugador3);
             }
             else
             {
-                // 2,  luego 3, luego 1
+                Debug.Log("La velocidad del jugador 2, " + velocidadJugador2 + " es la más rapida, después la del 3: " + velocidadJugador3 + " y despues la del 1:" + velocidadJugador1);
             }
         }
         else if (velocidadJugador3 >  velocidadJugador2 && velocidadJugador3 > velocidadJugador1)
         {
-            // mas rapido el 3
+            Debug.Log("La velocidad del jugador 3, " + velocidadJugador3 + " es la más rapida");
             if (velocidadJugador2 > velocidadJugador1)
             {
-                //3, luego 2, luego 1
+                Debug.Log("La velocidad del jugador 3, " + velocidadJugador3 + " es la más rapida, después la del 2: " + velocidadJugador2 + " y despues la del 1:" + velocidadJugador1);
             }
             else
             {
-                //3, luego 1, luego 2
+                Debug.Log("La velocidad del jugador 3, " + velocidadJugador3 + " es la más rapida, después la del 1: " + velocidadJugador1 + " y despues la del 2:" + velocidadJugador2);
             }
         }
     }
--- Assets/Scripts/Ej5_8.cs
+++ Assets/Scripts/Ej5_8.cs
@@ -1,13 +1,22 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ej5_8 : MonoBehaviour
 {
+    [SerializeField] int H, M, S;
+    //86399
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (((H*3600) + (M*59) + S)<86399)
+        {
+            Debug.Log("La hora introducida es válida: " + H + ": " + M + ":" + S);
+        }
+        if (((H * 3600) + (M * 59) + S) > 86399)
+        {
+            Debug.Log("La hora introducida no es válida: " + H + ": " + M + ":" + S);
+        }
     }
 
     // Update is called once per frame
